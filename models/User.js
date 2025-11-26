07fe7ba8422684ca4e493cadda9023868b656e17
taiko <pa.quilodran@duocuc.cl>
2025-11-26 02:45:04 -0300

    no doy más papito

--- models/User.js
+++ models/User.js
@@ -7,16 +7,15 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
+// Hashear la contraseña ANTES de guardar
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
-
+    if (!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
 
+// Comparar contraseña con bcrypt
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
